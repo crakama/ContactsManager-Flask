b370a44d1eef27b1940e6e5d2c9c4fe112031762
Catherine Rakama <crakama89@gmail.com>
2016-02-18 14:21:55 +0300

    create db scripts and db-migration sripts

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 contactsapp = Flask(__name__)
 contactsapp.config.from_object('config')
+db = SQLAlchemy(contactsapp)
 
-from app import routes
+
+from app import routes, models
--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
+import re
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'SKEY'
+
 
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
