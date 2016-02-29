16ce9c25874f8460c0461c8dd7b5d6986e7dc28a
Catherine Rakama <crakama89@gmail.com>
2016-02-29 12:53:48 +0300

    Fisrt Deploy

--- app/main/views.py
+++ app/main/views.py
@@ -10,6 +10,8 @@ from ..models import User
 
 @main.route('/')
 def index():
+	if not session['logged_in']:
+		return redirect(url_for('auth.login'))
 	return render_template('index.html')
 
 @main.route('/user/<name>')
